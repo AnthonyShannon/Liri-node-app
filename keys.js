2569aea755df9aaf0584bb4ecf841d0cf8907a9d
John Calvin <ashannon1990@gmail.com>
2019-04-29 18:17:59 -0400

    omdb working

--- keys.js
+++ keys.js
@@ -2,3 +2,9 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+exports.omdbapi = {
+    id: process.env.OMDB_ID
+}
+exports.bandsInTown = {
+  id: process.env.BANDSINTOWN_ID
+}
--- liri.js
+++ liri.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+
 
 let whatToDo = process.argv[2]
 let searchQuery = process.argv.slice(3).join(" ")
@@ -22,4 +24,22 @@ switch (whatToDo) {
                 console.log(err);
             });
         break;
+    case 'OMDB':
+        axios
+            .get("http://www.omdbapi.com/?t=" + searchQuery + "&apikey=62771a8e")
+            .then(function (response) {
+                console.log("Title: " + response.data.Title)
+                console.log("Year: " + response.data.Year)
+                console.log("IMDB Rating: " + response.data.Ratings[0].Value)
+                console.log("Rotton Tomatoes Rating: " + response.data.Ratings[1].Value)
+                console.log("Country of production: " + response.data.Country)
+                console.log("Plot: " + response.data.Plot)
+                console.log("Actors: " + response.data.Actors)
+                
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+
 }
